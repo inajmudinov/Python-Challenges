e0508463426370db88c59c82fb193a66caf0442a
Ilkhom <inajmudinov@yahoo.com>
2020-03-16 06:02:29 -0700

    Solved Challenge

---
+++ Hacker_rank_leap_year.py
@@ -0,0 +1,10 @@
+def leap_year(n):
+    if n % 400 == 0:
+        return True
+    if n % 100 == 0:
+        return False
+    if n % 4 == 0:
+        return True
+    else:
+        return False
+print (leap_year(1900))
